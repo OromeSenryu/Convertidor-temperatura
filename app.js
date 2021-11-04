a5b89ce9331924db564358ec229dff19509cfbf7
OromeSenryu <70779127+OromeSenryu@users.noreply.github.com>
2021-11-03 18:07:09 -0600

    Se agregan archivos base de proyecto

---
+++ app.js
@@ -0,0 +1,7 @@
+// Preguntar la temperatura en Celcius
+let temperature = prompt('¿Cuál es la temperatura en Celsius?');
+// Convertir Celsius a Fahrenheit
+let convertToFahrenheit = (temperature*1.8) + 32;
+// Mostrar resultado en la consola y en la web
+ console.log('La temperatura en Fahrenheit es ' + convertToFahrenheit);
+ document.write('La temperatura en Fahrenheit es ' + convertToFahrenheit);
